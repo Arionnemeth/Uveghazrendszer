02e5023d8ea2fcd4b5593714d0ac7071a80341c7
Arion <Arion@LENOVO-ARION>
2026-02-20 09:27:37 +0100

    Novenyfaj osztaly kesz

--- Uveghazrendszer/NovenyFaj.cs
+++ Uveghazrendszer/NovenyFaj.cs
@@ -6,5 +6,29 @@ namespace Uveghazrendszer
 {
     internal class NovenyFaj
     {
+        string nev;
+        int nedvességtartalom; // 100 az ideális
+        int egeszsegszint; // 100 a max
+        int optimalisSuruseg;  
+
+        public NovenyFaj(string nev, int optimalisSuruseg)
+        {
+            this.nev = nev;
+            this.optimalisSuruseg = optimalisSuruseg;
+            this.nedvességtartalom = 100;
+            this.egeszsegszint = 100;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Nedvességtartalom { get => nedvességtartalom; set => nedvességtartalom = value; }
+        public int Egeszsegszint { get => egeszsegszint; set => egeszsegszint = value; }
+        public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+
+        public string Azonosito { get { return this.nev.Substring(0, 3); } }
+
+        public override string ToString()
+        {
+            return $"{this.nev} nedvesseg: {this.nedvességtartalom} egeszseg: {this.egeszsegszint} ideális egyedszám: {this.optimalisSuruseg}";
+        }
     }
 }
