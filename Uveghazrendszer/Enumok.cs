fe12b67016b5f396e175da6b2b43508655e61cbd
Arion <Arion@LENOVO-ARION>
2026-02-20 09:16:32 +0100

    Kezelo osztaly kesz

---
+++ Uveghazrendszer/Enumok.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uveghazrendszer
+{
+    enum Szerepkor
+    {
+        KERTESZ,
+        TECHNIKUS,
+        ADMIN
+    }
+
+
+
+    internal class Enumok
+    {
+    }
+}
